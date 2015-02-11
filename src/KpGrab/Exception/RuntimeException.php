8a38409809173908ec4ee9eb26808b12b11d4ca8
点圈点圈点圈 <112367@qq.com>
2015-02-11 09:40:01 +0800

    update file title

--- src/KpGrab/Exception/RuntimeException.php
+++ src/KpGrab/Exception/RuntimeException.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * Kittencup
  *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @date 2015 15/1/30 上午11:44
+ * @copyright Copyright (c) 2014-2015 Kittencup. (http://www.kittencup.com)
+ * @license   http://kittencup.com
  */
-
 namespace KpGrab\Exception;
 
 /**
